4fb0b888c56543f89d57af98b546462800c1815f
David Cooper <48769207+dcooperdev@users.noreply.github.com>
2026-02-02 15:24:26 -0300

    fix: disable puppeteer sandbox for CI

--- generate.js
+++ generate.js
@@ -3,7 +3,10 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({
+        headless: 'new',
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
 
     // Carga el archivo local
